4f815d537e3034ece19e08050224b309e74c95c6
ehabfeki <ehab.shaaban@contentsquare.com>
2022-11-08 18:33:02 +0100

    chore: Refactor

--- get_unit_tests.py
+++ get_unit_tests.py
@@ -6,20 +6,16 @@
     - [x] trim testcase names > it( > @Test(
 """
 
-
 import glob
 import mmap
 from cli_args_system import Args
 
-args = Args(convert_numbers=False)
+anomalies = [b"$emit", b"submit(", b"commit(", b"split(", b"@Test(\n", b"[C", b", 'C"]
+snippets = [b'it(', b'@Test(', b', async () => {', b', assert', b', () => {', b',  () => {', b', () =>', b', fakeAsync(() => {', b', doneFn => {', b', done => {', b', async doneFn => {', b', async done => {', b', async(() => {', b', {', b", '')", b'Rendered);', b'Significant);', b'Zero);', b' expect(isFovExtendable(mockFovFilter)).toBeFalsy());', b', function () {});', b'EmptyContentIs', b'LoaderIs', b'ErrorBoundaryIs', b'FiltersBarMainIs', b'FiltersBarDrilldownIs', b'FiltersBarPageIs', b'ComponentIs', b'ComponentIsNot', b'LineChartIs', b", '' skip: true })", b'// ', b"t close menu'"]
 
-# "/home/cerebral/cs/app-frontends/apps/"
-path = args.flag_str('p','path')
-
-def units_fetcher():
+def units_fetcher(path):
     raw_units = []
     specs = glob.glob(path+'**/*.spec.ts', recursive=True)
-
     for spec in specs:
         with open(spec, 'rb') as file:
             f = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
@@ -30,103 +26,38 @@ def units_fetcher():
                     raw_units.append(line)
     return raw_units, len(raw_units)
 
-def units_cleaner(raw_units):
+def anomalies_remover(raw_units):
     units = []
-
-    for r_u in raw_units:
-        if (b"$emit") in r_u:
-            raw_units.remove(r_u)
-        elif (b"submit(") in r_u:
-            raw_units.remove(r_u)
-        elif (b"commit(") in r_u:
-            raw_units.remove(r_u)
-        elif (b"split(") in r_u:
-            raw_units.remove(r_u)
-        elif (b"@Test(\n") in r_u:
-            raw_units.remove(r_u)
-        elif (b"[C") in r_u:
-            raw_units.remove(r_u)
-        elif (b", 'C") in r_u:
-            raw_units.remove(r_u)
-        else:
-            if (b', async () => {') in r_u:
-                r_u = r_u.replace(b', async () => {', b'')
-                units.append(r_u.strip())
-            elif (b', assert') in r_u:
-                r_u_index = r_u.find(b', assert')
-                r_u = r_u.rstrip(r_u[r_u_index:])
-                units.append(r_u.strip())
-            elif (b', () => {') in r_u: 
-                r_u = r_u.replace(b', () => {', b'')
-                units.append(r_u.strip())
-            elif (b',  () => {') in r_u: 
-                r_u = r_u.replace(b',  () => {', b'')
-                units.append(r_u.strip())
-            elif (b', () =>') in r_u: 
-                r_u = r_u.replace(b', () =>', b'')
-                units.append(r_u.strip())
-            elif (b', fakeAsync(() => {') in r_u: 
-                r_u = r_u.replace(b', fakeAsync(() => {', b'')
-                units.append(r_u.strip())
-            elif (b', doneFn => {') in r_u: 
-                r_u = r_u.replace(b', doneFn => {', b'')
-                units.append(r_u.strip())
-            elif (b', done => {') in r_u: 
-                r_u = r_u.replace(b', done => {', b'')
-                units.append(r_u.strip())
-            elif (b', async doneFn => {') in r_u: 
-                r_u = r_u.replace(b', async doneFn => {', b'')
-                units.append(r_u.strip())
-            elif (b', async done => {') in r_u: 
-                r_u = r_u.replace(b', async done => {', b'')
-                units.append(r_u.strip())
-            elif (b', async(() => {') in r_u: 
-                r_u = r_u.replace(b', async(() => {', b'')
-                units.append(r_u.strip())
-            elif (b', {') in r_u: 
-                r_u = r_u.replace(b', {', b'')
-                units.append(r_u.strip())
-            elif (b", '')") in r_u: 
-                r_u = r_u.replace(b", '')", b'')
-                units.append(r_u.strip())
-            elif (b'Rendered);') in r_u: 
-                r_u = r_u.replace(b'Rendered);', b'')
-                units.append(r_u.strip())
-            elif (b'Significant);') in r_u: 
-                r_u = r_u.replace(b',Significant);', b'')
-                units.append(r_u.strip())
-            elif (b'Zero);') in r_u: 
-                r_u = r_u.replace(b'Zero);', b'')
-                units.append(r_u.strip())
-            elif (b', () => expect(isFovExtendable(mockFovFilter)).toBeFalsy());') in r_u: 
-                r_u = r_u.replace(b', () => expect(isFovExtendable(mockFovFilter)).toBeFalsy());', b'')
-                units.append(r_u.strip())
-            else:
-                units.append(r_u.strip())
-                
+    for a in anomalies:
+        raw_units = [r_u for r_u in raw_units if a not in r_u ]
+    units = raw_units
     return units, len(units)
 
-def units_glamourer(units):
-    glamourered_units = []
-    for u in units:
-        u = u.replace(b'it(', b'')
-        u = u.replace(b'@Test(', b'')
-        glamourered_units.append(u)
-    return glamourered_units, len(glamourered_units)
+def snippets_remover(units):
+    for idx in range(len(units)):
+        for s in snippets:
+            if s in units[idx]:
+                units[idx] = units[idx].replace(s, b'')
+        units[idx] = units[idx].strip()
+    return units
 
 if __name__ == "__main__":
-    raw_units, raw_units_total = units_fetcher()
-    units, units_total = units_cleaner(raw_units)
-    glamourered_units, glamourered_units_total = units_glamourer(units)
+    args = Args(convert_numbers=False)
 
-    f = open("units.log", "a")
-    
-    for g_u in glamourered_units:
-        f.write(g_u.decode("utf-8"))
-        f.write("\n")
+    # Example: "/home/cerebral/cs/app-frontends/apps/"
+    path = args.flag_str('p','path')
+
+    raw_units, raw_units_total = units_fetcher(path)
+    units, units_total = anomalies_remover(raw_units)
+    units = snippets_remover(units)
 
     print("raw_units_total", raw_units_total)
     print("units_total", units_total)
-    print("glamourered_units_total", glamourered_units_total)
+
+    f = open("units.log", "a")
+    
+    for u in units:
+        f.write(u.decode("utf-8"))
+        f.write("\n")
 
     f.close()
